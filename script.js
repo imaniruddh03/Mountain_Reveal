f1cf6b3978ab221bca1e375a826f6779da0a0acf
Aniruddh Shrivastava <89998419+imaniruddh03@users.noreply.github.com>
2023-07-20 00:57:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '100px',
+    duration: '2500',
+    delay:'300',
+ });
+
+ ScrollReveal().reveal('.main-tittle ,.section-title', { delay: 500 , origin: 'left' });
+ ScrollReveal().reveal('.sec-01 .image', { delay: 700 , origin: 'bottom' });
+ ScrollReveal().reveal('.textbox', { delay: 800 , origin: 'right' });
+ ScrollReveal().reveal('.media-content i', { delay: 800 , origin: 'bottom' , interval:400 });
+ ScrollReveal().reveal('.sec-02 .image , ', { delay: 500 , origin: 'top' });
+ ScrollReveal().reveal('.info', { delay: 600 , origin: 'left' });
+ ScrollReveal().reveal('.sec-03 .image', { delay: 600 , origin: 'right' });
+ ScrollReveal().reveal('.media-info li', { delay: 800 , origin: 'left' , interval:400 });
+
+ 
+ 
